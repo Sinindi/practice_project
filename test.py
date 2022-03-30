13da9c310f366c0909491a4539c628f23deab61d
Sinindi <hi8519a@naver.com>
2022-03-30 18:02:54 +0900

    preprocessing

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-age = "20"
-print( age * 2)
-print(age + 5)
+res = 0
+for i in range(2,10,3):
+    for fruit in ['사과', '딸기', '바나나']:
+        if fruit =='딸기':
+            continue
+        res += i
+        print(f'i ={i}, res = {res}')
+        
